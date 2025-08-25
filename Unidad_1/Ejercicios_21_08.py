c71bbae41def8191735d79abf08448d4cd15ce0f
sabrinagimenez98 <sabrinita9898@gmail.com>
2025-08-25 20:23:54 -0300

    Escritura de consignas

---
+++ Unidad_1/Ejercicios_21_08.py
@@ -0,0 +1,22 @@
+#Ejercicio 6: Un alumno desea saber cuál será su calificación final en la materia de Algoritmos. 
+#Dicha calificación se compone de los siguientes porcentajes:
+#55% del promedio de sus tres calificaciones parciales.
+#30% de la calificación del examen final.
+#15% de la calificación de un trabajo final.
+
+#Ejercicio 7: Conversión de divisas
+#Un programa que lea un monto en dólares y lo convierta a pesos colombianos, argentinos y euros usando tasas de cambio fijas definidas en el código.
+
+
+#Ejercicio 8: Viaje en auto
+#Un auto consume 8 litros cada 100 km. El usuario ingresa la distancia en km y el precio de la gasolina por litro.
+#El programa debe calcular:
+#cuántos litros se necesitan,
+#cuánto costará el viaje,
+#y cuántas horas tardará si la velocidad promedio es de 90 km/h.
+
+#Ejercicio 9: Préstamo bancario
+#Un cliente solicita un préstamo que deberá pagar en 12 meses con interés fijo mensual del 2%.
+#El usuario ingresa el monto solicitado, y el programa debe calcular:
+#el monto total a devolver,
+#el valor de cada cuota mensual.
